3536cf78ec00a6c34e21785e82bf2f8350c2bd77
KvoERR <kvkazak3@gmail.com>
2024-12-22 23:25:31 +0500

    1

---
+++ recur/recur/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int n, x;
+float s=0;
+float rec(int n)
+{
+	if (n == -1)
+		return s;
+	s += (pow(-1, n) * (pow(x, 2 * n + 1) / (2 * n + 1)));
+	return rec(n - 1);
+}
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	cout << "¬ведите количество элементов последовательности: ";
+	cin >> n;
+	cout << "¬ведите значение переменной x: ";
+	cin >> x;
+	cout <<"S = ";
+	cout<<rec(n);
+}
